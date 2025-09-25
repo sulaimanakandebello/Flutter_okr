d6f197b8c764f4926c0c6a0a645a0d29fe15928e
sulaimanakandebello <slybee1@gmail.com>
2025-09-25 19:14:20 +0200

    Newest- working

--- lib/services/products_repository.dart
+++ lib/services/products_repository.dart
@@ -1,25 +1,3 @@
-/*
-// lib/services/products_repository.dart
-import '../models/product.dart';
-
-abstract class ProductsRepository {
-  Future<List<Product>> fetchFeed({int limit = 20, String? categoryFilter});
-  Future<List<Product>> fetchSellerItems(String sellerId, {int limit = 20});
-  Future<List<Product>> fetchSimilar(String categoryPath, {int limit = 20});
-
-  Future<void> toggleLike({
-    required String productId,
-    required String userId,
-  });
-
-  Future<String> createProduct(Product product);
-
-  Stream<Product> watchProduct(String id, {String? currentUserId});
-}
-
-
-*/
-
 // lib/services/products_repository.dart
 import '../models/product.dart';
 
